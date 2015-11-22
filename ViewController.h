b2a9fb96acba509dfac9cba55e812dcd95884920
Alexander Harrison <alexkendall.harrison@gmail.com>
2015-11-21 22:45:28 -0500

    First commit

---
+++ ViewController.h
@@ -0,0 +1,46 @@
+//
+//  ViewController.h
+//  Practice
+//
+//  Created by Alex Harrison on 11/21/15.
+//  Copyright © 2015 Alex Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EZAudioFile.h"
+#import "EZAudioPlayer.h"
+#import "EZAudio.h"
+
+@interface ViewController : UIViewController
+
+@property UIView* super_view;
+@property UIButton* play_button;
+@property UILabel *title_label;
+@property UILabel *artist_label;
+@property BOOL playing;
+
+#define kAudioFileDefault [[NSBundle mainBundle] pathForResource:@"lunar" ofType:@"MP3"]
+
+/**
+ An EZAudioFile that will be used to load the audio file at the file path specified
+ */
+@property (nonatomic, strong) EZAudioFile *audioFile;
+
+//------------------------------------------------------------------------------
+
+/**
+ An EZAudioPlayer that will be used for playback
+ */
+@property (nonatomic, strong) EZAudioPlayer *player;
+
+//------------------------------------------------------------------------------
+
+/**
+ The CoreGraphics based audio plot
+ */
+@property EZAudioPlot *audioPlot;
+
+//------------------------------------------------------------------------------
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,465 @@
+//
+//  ViewController.m
+//  Practice
+//
+//  Created by Alex Harrison on 11/21/15.
+//  Copyright © 2015 Alex Harrison. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "EZAudioFile.h"
+#import "EZAudioPlayer.h"
+#import "EZAudioPlot.h"
+@import UIKit;
+@import SceneKit;
+
+@interface ViewController ()
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    //**************************************************************************************
+    //
+    // Setup the AVAudioSession. EZMicrophone will not work properly on iOS
+    // if you don't do this!
+    //
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    NSError *error;
+    [session setCategory:AVAudioSessionCategoryPlayback error:&error];
+    if (error)
+    {
+        NSLog(@"Error setting up audio session category: %@", error.localizedDescription);
+    }
+    [session setActive:YES error:&error];
+    if (error)
+    {
+        NSLog(@"Error setting up audio session active: %@", error.localizedDescription);
+    }
+    //**************************************************************************************
+    
+    // configure superview
+    self.super_view = self.view;
+    self.super_view.backgroundColor = [UIColor blackColor];
+    
+    UIColor *dark_gray = [[UIColor alloc]initWithRed:0.12 green:0.12 blue:0.12 alpha:1.0];
+    
+    // configure scene view
+    SCNView *scene_view = [[SCNView alloc]initWithFrame:self.view.bounds];
+    scene_view.antialiasingMode = SCNAntialiasingModeMultisampling4X;
+    scene_view.backgroundColor = dark_gray;
+    [self.view addSubview:scene_view];
+    
+    // scene
+    SCNScene *scene = [[SCNScene alloc]init];
+    scene_view.scene = scene;
+    
+    // camera
+    SCNCamera *camera = [[SCNCamera alloc]init];
+    SCNNode *camera_node = [[SCNNode alloc]init];
+    camera_node.camera = camera;
+    
+    // spot light
+    SCNLight *spot_light = [[SCNLight alloc]init];
+    spot_light.type = SCNLightTypeSpot;
+    spot_light.spotInnerAngle = 0.0;
+    spot_light.spotOuterAngle = 90.0;
+    spot_light.castsShadow = true;
+    SCNNode *spot_node = [[SCNNode alloc]init];
+    spot_node.light = spot_light;
+    spot_node.position = SCNVector3Make(0.0, 10.0, 0.0);
+
+    // ambient light
+    SCNLight *ambient_light = [[SCNLight alloc]init];
+    ambient_light.type = SCNLightTypeAmbient;
+    ambient_light.color = [UIColor colorWithRed:0.5 green:0.5 blue:0.5 alpha:1.0];
+    camera_node.light = ambient_light;
+    
+    // configure spheres
+    CGFloat step = 3.0;
+    CGFloat origin_x = -6.0;
+    int num_rows = 5;
+    int num_cols = 5;
+    NSMutableArray *spheres = [[NSMutableArray alloc]init];
+    
+    for(int r = 0; r < num_rows; ++r)
+    {
+        for(int c = 0; c < num_cols; ++c)
+        {
+            // sphere geometry
+            SCNSphere *sphere_geometry = [[SCNSphere alloc]init];
+            sphere_geometry.radius = 1.0;
+            
+            // sphere node
+            SCNNode *sphere_node = [[SCNNode alloc]init];
+            sphere_node.geometry = sphere_geometry;
+            
+            // sphere material
+            SCNMaterial *sphere_material = [[SCNMaterial alloc]init];
+            sphere_material.diffuse.contents = [UIColor greenColor];
+            
+            NSArray *materials = @[sphere_material];
+            sphere_geometry.materials = materials;
+            
+            CGFloat x = origin_x + (c * step);
+            CGFloat y = r * step;
+            sphere_node.position = SCNVector3Make(x, 0.5, y);
+            [scene.rootNode addChildNode:sphere_node];
+            [spheres addObject:sphere_node];
+        }
+    }
+    
+    // generate floor
+    SCNPlane *floor_geometry = [[SCNPlane alloc]init];
+    floor_geometry.width = 1000.0;
+    floor_geometry.height = 1000.0;
+    
+    SCNNode *floor_node = [[SCNNode alloc]init];
+    floor_node.geometry = floor_geometry;
+    floor_node.eulerAngles = SCNVector3Make(GLKMathDegreesToRadians(-90), 0, 0);
+    floor_node.position = SCNVector3Make(0.0, -0.5, 0.0);
+    
+    // sphere material
+    SCNMaterial *floor_material = [[SCNMaterial alloc]init];
+    UIColor *dark_gray_color = [[UIColor alloc]initWithRed:0.25 green:0.25 blue:0.25 alpha:1.0];
+    
+    floor_material.diffuse.contents = dark_gray_color;
+    
+    NSArray *materials = @[floor_material];
+    floor_geometry.materials = materials;
+    [scene.rootNode addChildNode:floor_node];
+    [scene.rootNode addChildNode:spot_node];
+    [scene.rootNode addChildNode:camera_node];
+    
+    // configure camera position and eye
+    camera_node.position = SCNVector3Make(0.0, 10.0, 35.0);
+    SCNLookAtConstraint *constraint = [SCNLookAtConstraint lookAtConstraintWithTarget:spheres[12]];
+    constraint.gimbalLockEnabled = true;
+    NSArray *constraints = @[constraint];
+    camera_node.constraints = constraints;
+    spot_node.constraints = constraints;
+
+    
+    // configure play button
+    CGFloat width = self.super_view.bounds.size.width * 0.5;
+    CGFloat height = self.super_view.bounds.size.height * 0.05;
+    CGFloat offset_x = (self.super_view.bounds.size.width - width) * 0.5;
+    CGFloat offset_y = self.super_view.bounds.size.height * 0.05;
+    
+    // configure play button
+    self.play_button = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.play_button.frame = CGRectMake(offset_x, offset_y, width, height);
+    self.play_button.backgroundColor = [UIColor whiteColor];
+    self.play_button.layer.cornerRadius = height * 0.15;
+    
+    // configure title properties
+    [self.play_button setTitle:@"Play" forState:UIControlStateNormal];
+    [self.play_button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [self.play_button setTitleColor:[UIColor grayColor] forState:UIControlStateHighlighted];
+    [self.super_view addSubview:self.play_button];
+    
+    // add targets for playing song
+    [self.play_button addTarget:self action:@selector(play_song) forControlEvents:UIControlEventTouchUpInside];
+    self.playing = false;
+    
+    // configure audio plot
+    CGFloat plot_height = self.view.bounds.size.height * 0.2;
+    CGFloat plot_width = self.view.bounds.size.width;
+    CGFloat plot_y = self.view.bounds.size.height - plot_height;
+    
+    //
+    self.audioPlot = [[EZAudioPlot alloc] initWithFrame:CGRectMake(0.0, plot_y, plot_width, plot_height)];
+    self.audioPlot.frame = CGRectMake(0.0, plot_y, plot_width, plot_height);
+    
+    // configure plot visuals
+    self.audioPlot.backgroundColor = [UIColor clearColor];
+    self.audioPlot.color           = [UIColor whiteColor];
+    self.audioPlot.plotType        = EZPlotTypeRolling;
+    self.audioPlot.shouldFill      = NO;
+    self.audioPlot.shouldMirror    = YES;
+    
+    // add plot to subview
+    [self.super_view addSubview:self.audioPlot];
+    
+    
+    // configure song title label
+    CGFloat label_height = 40.0;
+    CGFloat label_width = self.view.bounds.size.width;
+    CGFloat title_y = self.view.bounds.size.height * 0.2;
+    self.title_label = [[UILabel alloc]initWithFrame:CGRectMake(0.0, title_y, label_width, label_height)];
+    [self.title_label setText:@"Welcome to Lunar Industries"];
+    [self.title_label setTextColor:[UIColor whiteColor]];
+    [self.title_label setTextAlignment:NSTextAlignmentCenter];
+    self.title_label.font = [UIFont boldSystemFontOfSize:18.0];
+    
+    // configure song artest label
+    CGFloat artist_y = title_y + 30.0;
+    self.artist_label = [[UILabel alloc]initWithFrame:CGRectMake(0.0, artist_y, label_width, label_height)];
+    [self.artist_label setText:@"Clint Mansell"];
+    [self.artist_label setTextColor:[UIColor whiteColor]];
+    [self.artist_label setTextAlignment:NSTextAlignmentCenter];
+    self.artist_label.font = [UIFont systemFontOfSize:17.0];
+    
+    
+    [self.super_view addSubview:self.title_label];
+    [self.super_view addSubview:self.artist_label];
+    
+    
+    //**************************************************************************************
+    
+    NSLog(@"outputs: %@", [EZAudioDevice outputDevices]);
+    
+    //
+    // Create the audio player
+    //
+    self.player = [EZAudioPlayer audioPlayerWithDelegate:self];
+    self.player.shouldLoop = YES;
+    
+    // Override the output to the speaker
+    [session overrideOutputAudioPort:AVAudioSessionPortOverrideSpeaker error:&error];
+    if (error)
+    {
+        NSLog(@"Error overriding output to the speaker: %@", error.localizedDescription);
+    }
+    
+    //
+    // Customize UI components
+    //
+    //self.rollingHistorySlider.value = (float)[self.audioPlot rollingHistoryLength];
+    
+    //
+    // Listen for EZAudioPlayer notifications
+    //
+    [self setupNotifications];
+    
+    /*
+     Try opening the sample file
+     */
+    [self openFileWithFilePathURL:[NSURL fileURLWithPath:kAudioFileDefault]];
+
+    
+    //**************************************************************************************
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void)play_song
+{
+    if(self.playing)
+    {
+        
+        printf("pausing song\n");
+        [self.play_button setTitle:@"Play" forState:UIControlStateNormal];
+        self.playing = false;
+        [self.player pause];
+    }
+    else
+    {
+        printf("playing song\n");
+        [self.play_button setTitle:@"Pause" forState:UIControlStateNormal];
+        self.playing = true;
+        [self.player play];
+        
+    }
+}
+
+//------------------------------------------------------------------------------
+#pragma mark - Notifications
+//------------------------------------------------------------------------------
+
+- (void)setupNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(audioPlayerDidChangeAudioFile:)
+                                                 name:EZAudioPlayerDidChangeAudioFileNotification
+                                               object:self.player];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(audioPlayerDidChangeOutputDevice:)
+                                                 name:EZAudioPlayerDidChangeOutputDeviceNotification
+                                               object:self.player];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(audioPlayerDidChangePlayState:)
+                                                 name:EZAudioPlayerDidChangePlayStateNotification
+                                               object:self.player];
+}
+
+//------------------------------------------------------------------------------
+
+- (void)audioPlayerDidChangeAudioFile:(NSNotification *)notification
+{
+    EZAudioPlayer *player = [notification object];
+    NSLog(@"Player changed audio file: %@", [player audioFile]);
+}
+
+//------------------------------------------------------------------------------
+
+- (void)audioPlayerDidChangeOutputDevice:(NSNotification *)notification
+{
+    EZAudioPlayer *player = [notification object];
+    NSLog(@"Player changed output device: %@", [player device]);
+}
+
+//------------------------------------------------------------------------------
+
+- (void)audioPlayerDidChangePlayState:(NSNotification *)notification
+{
+    EZAudioPlayer *player = [notification object];
+    NSLog(@"Player change play state, isPlaying: %i", [player isPlaying]);
+}
+
+//------------------------------------------------------------------------------
+#pragma mark - Actions
+//------------------------------------------------------------------------------
+
+- (void)changePlotType:(id)sender
+{
+    NSInteger selectedSegment = [sender selectedSegmentIndex];
+    switch(selectedSegment)
+    {
+        case 0:
+            [self drawBufferPlot];
+            break;
+        case 1:
+            [self drawRollingPlot];
+            break;
+        default:
+            break;
+    }
+}
+
+
+- (void)openFileWithFilePathURL:(NSURL *)filePathURL
+{
+    //
+    // Create the EZAudioPlayer
+    //
+    self.audioFile = [EZAudioFile audioFileWithURL:filePathURL];
+    
+    //
+    // Update the UI
+    //
+    
+    
+    //self.filePathLabel.text = filePathURL.lastPathComponent;
+    //self.positionSlider.maximumValue = (float)self.audioFile.totalFrames;
+    //self.volumeSlider.value = [self.player volume];
+    
+    //
+    // Plot the whole waveform
+    //
+    self.audioPlot.plotType = EZPlotTypeBuffer;
+    self.audioPlot.shouldFill = YES;
+    self.audioPlot.shouldMirror = YES;
+    __weak typeof (self) weakSelf = self;
+    [self.audioFile getWaveformDataWithCompletionBlock:^(float **waveformData,
+                                                         int length)
+     {
+         [weakSelf.audioPlot updateBuffer:waveformData[0]
+                           withBufferSize:length];
+     }];
+    
+    //
+    // Play the audio file
+    //
+    [self.player setAudioFile:self.audioFile];
+    
+}
+
+//------------------------------------------------------------------------------
+
+- (void)seekToFrame:(id)sender
+{
+    [self.player seekToFrame:(SInt64)[(UISlider *)sender value]];
+}
+
+//------------------------------------------------------------------------------
+#pragma mark - EZAudioPlayerDelegate
+//------------------------------------------------------------------------------
+
+- (void)  audioPlayer:(EZAudioPlayer *)audioPlayer
+          playedAudio:(float **)buffer
+       withBufferSize:(UInt32)bufferSize
+ withNumberOfChannels:(UInt32)numberOfChannels
+          inAudioFile:(EZAudioFile *)audioFile
+{
+    __weak typeof (self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [weakSelf.audioPlot updateBuffer:buffer[0]
+                          withBufferSize:bufferSize];
+    });
+    
+}
+
+//------------------------------------------------------------------------------
+
+- (void)audioPlayer:(EZAudioPlayer *)audioPlayer
+    updatedPosition:(SInt64)framePosition
+        inAudioFile:(EZAudioFile *)audioFile
+{
+    __weak typeof (self) weakSelf = self;
+    dispatch_async(dispatch_get_main_queue(), ^{
+        
+        /*
+        if (!weakSelf.positionSlider.touchInside)
+        {
+            weakSelf.positionSlider.value = (float)framePosition;
+        }
+        */
+    });
+    
+    
+}
+
+//------------------------------------------------------------------------------
+#pragma mark - Utility
+//------------------------------------------------------------------------------
+
+/*
+ Give the visualization of the current buffer (this is almost exactly the openFrameworks audio input eample)
+ */
+- (void)drawBufferPlot
+{
+    self.audioPlot.plotType = EZPlotTypeBuffer;
+    self.audioPlot.shouldMirror = NO;
+    self.audioPlot.shouldFill = NO;
+}
+
+//------------------------------------------------------------------------------
+
+/*
+ Give the classic mirrored, rolling waveform look
+ */
+- (void)drawRollingPlot
+{
+    self.audioPlot.plotType = EZPlotTypeRolling;
+    self.audioPlot.shouldFill = YES;
+    self.audioPlot.shouldMirror = YES;
+}
+
+//------------------------------------------------------------------------------
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
